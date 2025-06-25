a7c4147bb86310d38cd0ad982ecf5bf9a128df86
ifunhy <guswl8296@gmail.com>
2025-06-25 16:59:47 +0900

    Resolve merge conflicts

---
+++ src/BaekJoon/Class01/Ex10171.java
@@ -0,0 +1,10 @@
+package BaekJoon.Class01;
+
+public class Ex10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
--- src/BaekJoon/Class03/Ex1003.java
+++ src/BaekJoon/Class03/Ex1003.java
@@ -1,33 +1,58 @@
 package BaekJoon.Class03;
 import java.io.*;
 
-public class Ex1003{
-
-    public static void main(String[] args) throws IOException{
-
+public class Ex1003 {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 테스트 케이스 K
-        int K = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<K; i++) {
-            // 정수 N 받기
+        for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
-            // 초기값 세팅
-            int [][]result = new int[N+2][2];
-            result[0][0]=1;
-            result[0][1]=0;
-            result[1][0]=0;
-            result[1][1]=1;
-            for(int j=2; j<N+1; j++) {
-                result[j][0]= result[j-1][0]+result[j-2][0];
-                result[j][1]= result[j-1][1]+result[j-2][1];
+
+            int[][] result = new int[N+2][2];
+            result[0][0] = 1;
+            result[0][1] = 0;
+            result[1][0] = 0;
+            result[1][1] = 1;
+
+            for (int j = 2; j <= N; j++) {
+                result[j][0] = result[j-1][0] + result[j-2][0];
+                result[j][1] = result[j-1][1] + result[j-2][1];
             }
             System.out.println(result[N][0] + " " + result[N][1]);
         }
     }
-
 }
+
+
+
+//public class Ex1003{
+//
+//    public static void main(String[] args) throws IOException{
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        // 테스트 케이스 K
+//        int K = Integer.parseInt(br.readLine());
+//
+//        for(int i=0; i<K; i++) {
+//            // 정수 N 받기
+//            int N = Integer.parseInt(br.readLine());
+//            // 초기값 세팅
+//            int [][]result = new int[N+2][2];
+//            result[0][0]=1;
+//            result[0][1]=0;
+//            result[1][0]=0;
+//            result[1][1]=1;
+//            for(int j=2; j<N+1; j++) {
+//                result[j][0]= result[j-1][0]+result[j-2][0];
+//                result[j][1]= result[j-1][1]+result[j-2][1];
+//            }
+//            System.out.println(result[N][0] + " " + result[N][1]);
+//        }
+//    }
+//}
 
 //public class Ex1003 {
 //    public static void main(String[] args) throws IOException{
