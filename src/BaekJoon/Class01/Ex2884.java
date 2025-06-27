1ded1e045de855f549cb4804c633f9d2930b2ed5
ifunhy <guswl8296@gmail.com>
2025-06-27 17:47:00 +0900

    0627

--- src/BaekJoon/Class01/Ex2884.java
+++ src/BaekJoon/Class01/Ex2884.java
@@ -1,15 +1,10 @@
 package BaekJoon.Class01;
-<<<<<<< HEAD
-
-import java.io.*;
-import java.util.*;
-=======
 import java.io.*;
 import java.util.StringTokenizer;
->>>>>>> 20f4bd0 (test)
+
 
 public class Ex2884 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -17,23 +12,15 @@ public class Ex2884 {
         int M = Integer.parseInt(st.nextToken());
 
         if (M < 45) {
-<<<<<<< HEAD
             if (H == 0)
                 H = 23;
             else H--;
 
-=======
-            H--;
->>>>>>> 20f4bd0 (test)
             M = 60 - (45 - M);
-        }
-        else {
+        } else {
             M -= 45;
         }
         System.out.println(H + " " + M);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 20f4bd0 (test)
+
